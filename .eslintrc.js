3f2cf07d4c841c808d5b014f265bda652ddf3c87
Maksim <m_telega@mail.ru>
2022-03-24 09:44:56 +0300

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
