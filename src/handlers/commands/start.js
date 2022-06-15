c236d8f3311d4bcdce5d362414c9e26766d266cd
Maksim <m_telega@mail.ru>
2022-06-15 17:24:59 +0300

    Update start.js

--- src/handlers/commands/start.js
+++ src/handlers/commands/start.js
@@ -1,6 +1,7 @@
 const { Extra, Markup } = require('telegraf')
 const { BOT_USERNAME, BOT_REPO } = require('../../config')
-module.exports = ctx => {
+
+module.exports = async ctx => {
   return ctx.replyWithHTML(ctx.i18n.t('welcome.message'), Extra.markup(Markup.inlineKeyboard([
     [Markup.urlButton(ctx.i18n.t('welcome.buttons.github'), BOT_REPO)],
     [Markup.urlButton(ctx.i18n.t('welcome.buttons.addToChat'), `https://t.me/${BOT_USERNAME}?startgroup=add`)]
